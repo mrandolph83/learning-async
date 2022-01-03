4427d3edfcadbe6a7b41181abb5589488e96d29f
mrandolph83@gmail.com <mrandolph83@gmail.com>
2022-01-03 10:08:27 -0500

    Add a failHandler function and simulate fail status, leading to the loading of a default picture rather than Los Angeles weather data

--- begin/_scripts/ajax.js
+++ begin/_scripts/ajax.js
@@ -3,18 +3,27 @@
 let httpRequest = new XMLHttpRequest();
 
 // add get() function here
-function get(url, success) {
+function get(url, success, fail) {
     httpRequest.open('GET', url);
 
     httpRequest.onload = function() {
+        if (httpRequest.status === 200) {
         success(httpRequest.responseText);
+        } else {
+           fail(httpRequest.status); 
+        }
     }
-
     httpRequest.send();
 }
 
 function tempToF(kelvin) {
     return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+}
+
+function failHandler(status) {
+    console.log(status);
+    const weatherDiv = document.querySelector('#weather');
+    weatherDiv.classList.remove('hidden');
 }
 
 function successHandler(data) {
@@ -39,10 +48,12 @@ function successHandler(data) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = 'cc7cd487cdb6c27a6b71455a04520c3e'; // ADD YOUR API KEY BETWEEN THE QUOTES
+    // const apiKey = 'cc7cd487cdb6c27a6b71455a04520c3e'; // ADD YOUR API KEY BETWEEN THE QUOTES
+    const apiKey = ' '; // ADD YOUR API KEY BETWEEN THE QUOTES
+
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
     // add get() function call here
-    get(url, successHandler);
+    get(url, successHandler, failHandler);
     // successHandler(httpRequest.responseText);
     
 });
